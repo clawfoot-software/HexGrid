0d8e06a7ad0bd1d8903ad85299943197fa7c5942
Douglas <1400380+douglasg14b@users.noreply.github.com>
2020-01-22 13:53:31 -0800

    Makes Point X & Y public properties. Adds operators to Point

--- HexGrid/Point.cs
+++ HexGrid/Point.cs
@@ -7,7 +7,25 @@
             X = x;
             Y = y;
         }
-        public readonly double X;
-        public readonly double Y;
+
+        public double X { get; }
+        public double Y { get; }
+
+        public static bool operator ==(Point a, Point b) => a.X == b.X && a.Y == b.Y;
+        public static bool operator !=(Point a, Point b) => a.X != b.X || a.Y != b.Y;
+        public static Point operator +(Point a, Point b) => new Point(a.X + b.X, a.Y + b.Y);
+        public static Point operator -(Point a, Point b) => new Point(a.X - b.X, a.Y - b.Y);
+        public static Point operator *(Point a, Point b) => new Point(a.X * b.X, a.Y * b.Y);
+        public static Point operator /(Point a, Point b) => new Point(a.X / b.X, a.Y / b.Y);
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Point other))
+            {
+                return false;
+            }
+
+            return (Point)obj == this;
+        }
     }
 }
