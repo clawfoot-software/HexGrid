039b5cd2f7a9c18d0b2025e018cbaaa417c58246
Douglas <1400380+douglasg14b@users.noreply.github.com>
2020-01-22 14:15:49 -0800

    Adds static operator methods to Point to handle IPoint operations.
    Also adds IPoint interface

---
+++ HexGrid/Interfaces/IPoint.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Barbar.HexGrid.Interfaces
+{
+    public interface IPoint
+    {
+        double X { get; }
+        double Y { get; }
+    }
+}
--- HexGrid/Point.cs
+++ HexGrid/Point.cs
@@ -1,6 +1,8 @@
-﻿namespace Barbar.HexGrid
+﻿using Barbar.HexGrid.Interfaces;
+
+namespace Barbar.HexGrid
 {
-    public struct Point
+    public struct Point : IPoint
     {
         public Point(double x, double y)
         {
@@ -18,6 +20,11 @@
         public static Point operator *(Point a, Point b) => new Point(a.X * b.X, a.Y * b.Y);
         public static Point operator /(Point a, Point b) => new Point(a.X / b.X, a.Y / b.Y);
 
+        public static IPoint Add(IPoint a, IPoint b) => new Point(a.X + b.X, a.Y + b.Y);
+        public static IPoint Subtract(IPoint a, IPoint b) => new Point(a.X - b.X, a.Y - b.Y);
+        public static IPoint Multiply(IPoint a, IPoint b) => new Point(a.X * b.X, a.Y * b.Y);
+        public static IPoint Divide(IPoint a, IPoint b) => new Point(a.X / b.X, a.Y / b.Y);
+
         public override bool Equals(object obj)
         {
             if (!(obj is Point other))
@@ -26,6 +33,17 @@
             }
 
             return (Point)obj == this;
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                int hash = 17;
+                hash = hash * 23 + X.GetHashCode();
+                hash = hash * 23 + Y.GetHashCode();
+                return hash;
+            }
         }
     }
 }
